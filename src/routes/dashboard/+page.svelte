234d8e6831f692771648e347f2548d9c3d14bbb4
Eduardo Trevizo <eduardomttzt01@outlook.com>
2025-08-05 12:48:59 -0600

    fixing dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -22,7 +22,7 @@
 <div
 	class="flex min-h-screen items-center justify-center bg-gradient-to-br from-gray-900 to-black p-6 font-sans text-center text-gray-100"
 >
-	<div class="w-full max-w-lg space-y-8 rounded-xl bg-gray-800/50 p-10 shadow-xl backdrop-blur-sm">
+	<div class="w-full space-y-8 rounded-xl bg-gray-800/50 p-10 shadow-xl backdrop-blur-sm">
 		<h1 class="text-4xl font-extrabold text-white sm:text-5xl">¡Bienvenido {user?.email}!</h1>
 		<p class="text-lg text-gray-400">
 			Este es tu dashboard. Desde aquí puedes gestionar tu cuenta.
